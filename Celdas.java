bcc3358ee2e88de9d21f45885a004203b639734a
Derek <derekcortez16@gmail.com>
2019-05-03 02:37:42 -0500

    final

--- Celdas.java
+++ Celdas.java
@@ -1,3 +1,14 @@
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import java.util.Random;
+
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 public class Celdas {
     private boolean mina;
     private boolean bandera;
