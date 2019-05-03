396b47a44d40660cdbb8aed171730b3d1f94b805
Derek <derekcortez16@gmail.com>
2019-05-03 02:41:24 -0500

    :(

--- Tablero.java
+++ Tablero.java
@@ -1,4 +1,10 @@
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
 import java.util.Random;
+
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
