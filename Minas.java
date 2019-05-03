01273b297f9a8ee134e0d7019a6def977ca74ec7
Derek <derekcortez16@gmail.com>
2019-05-03 03:37:42 -0500

    AAAAAAAAA NO SE VE EL JUEGO

--- Minas.java
+++ Minas.java
@@ -1,33 +1,27 @@
-import java.awt.Graphics;
-import java.awt.Image;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
+import java.awt.BorderLayout;
 
-import java.util.Random;
-
-import javax.swing.BorderFactory;
-import javax.swing.ImageIcon;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JPanel;
 
 public class Minas extends JFrame {
-    private final int ANCHURA = 250;
-    private final int ALTURA = 300;
+    private final int ANCHURA = 500;
+    private final int ALTURA = 600;
 
     private JLabel barraStatus;
 
     public Minas() {
 
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
         setSize(ANCHURA, ALTURA);
         setLocationRelativeTo(null);
         setTitle("Buscaminas");
 
         barraStatus = new JLabel("");
         add(barraStatus, BorderLayout.SOUTH);
-        add(new Board(barraStatus));
+        add(new Tablero(barraStatus));
 
         setResizable(false);
-        setVisible(true);
+        
     }
 }
