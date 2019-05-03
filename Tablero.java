f69effb4b46db8bc5894725c117dba161d12faae
Derek <derekcortez16@gmail.com>
2019-05-03 01:37:28 -0500

    mas funciones olv

--- Tablero.java
+++ Tablero.java
@@ -107,4 +107,130 @@ public class Tablero extends JPanel {
         }
         return contador;
     }   
+    public void paint(Graphics g) {
+        int celdasCubiertas = 0;
+
+        for (int i = 0; i < this.filas; i++) {
+            for (int j = 0; j < this.columnas; j++) {
+                Celdas celda = this.celdas[i][j];
+                int tipoImagen;
+                int xPosition, yPosition;
+
+                if (celda.estaCubierto()) {
+                    celdasCubiertas++;
+                }
+
+                if (enCurso) {
+                    if (celda.esMina() && !celda.estaCubierto()) {
+                        enCurso = false;
+                    }
+                }
+
+                tipoImagen = this.decideTipo(celda);
+
+                xPosition = (j * TAM_CELDAS);
+                yPosition = (i * TAM_CELDAS);
+
+                g.drawImage(img[imageType], xPosition, yPosition, this);
+            }
+        }
+        if (coveredCells == 0 && inGame) {
+            inGame = false;
+            statusBar.setText("Game Won");
+        } else if (!inGame) {
+            statusBar.setText("Game Lost");
+        }
+    }
+    private int decideTipo(Celdas celda) {
+        int tipoImagen = celda.getValor();
+
+        if (!enCurso) {
+            if (celda.estaCubierto() && cell.esMina()) {
+                celda.descubierto();
+                imageType = IMAGEN_MINE;
+            } else if (cell.esBandera()) {
+                if (celda.esMina()) {
+                    tipoImagen = IMAGEN_BANDERA;
+                } else {
+                    tipoImagen = IMAGE_NO_BANDERA;
+                }
+            }
+        } else {
+            if (celda.esBandera()) {
+                tipoImagen = IMAGEN_BANDERA;
+            } else if (cell.isCovered()) {
+                tipoImagen = IMAGEN_COVER;
+            }
+        }
+
+        return tipoImagen;
+    }
+    public void encontrarCeldasVacias(int x, int y, int profundidad) {
+
+        for (int i = -1; i <= 1; ++i) {
+            int xIndex = x + i;
+
+            if (xIndex < 0 || xIndex >= this.filas) {
+                continue;
+            }
+
+            for (int j = -1; j <= 1; ++j) {
+                int yIndex = y + j;
+
+                if (yIndex < 0 || yIndex >= this.columnas) {
+                    continue;
+                }
+
+                if (!(i == 0 || j == 0)) {
+                    continue;
+                }
+
+                Celdas celda = this.celdas[xIndex][yIndex];
+                if (revisarVacio(celda)) {
+                    celda.descubierto();
+                    celda.marcada();
+
+                    celdasDescubiertasAlrededor(xIndex, yIndex);
+                    encontrarCeldasVacias(xIndex, yIndex, profundidad + 1);
+                }
+            }
+        }
+        if(profundidad==0){
+            this.limpiarCeldas();
+        }
+    }
+    private void celdasDescubiertasAlrededor(int x, int y) {
+        for (int i = -1; i <= 1; ++i) {
+            int xIndex = x + i;
+
+            if (xIndex < 0 || xIndex >= this.filas) {
+                continue;
+            }
+
+            for (int j = -1; j <= 1; ++j) {
+                int yIndex = y + j;
+
+                if (yIndex < 0 || yIndex >= this.columnas) {
+                    continue;
+                }
+
+                if (i == 0 || j == 0) {
+                    continue;
+                }
+
+                Celdas celda = this.celdas[xIndex][yIndex];
+                if (celda.estaCubierto() && !celda.vacio()) {
+                    celda.descubierto();
+                }
+            }
+        }
+    }
+    private boolean revisarVacio(Celda celda) {
+        if (!celda.esBandera()) {
+            if (celda.vacio()) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
